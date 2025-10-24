1f539a0a75023c30126158ed40775a9f21837c15
IdkLmao-119 <erickduldulao25@gmail.com>
2025-10-24 13:46:22 +0800

    Add files via upload

---
+++ Performance Task ( Team JCAEK)/Performance Task ( Team JCAEK)/LoginForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Performance_Task___Team_JCAEK_
+{
+    public partial class LoginForm: Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // insert Stream here for username/password.txt
+
+
+
+            /*
+             
+            if (Username == valid && Password == valid) {
+            initialize main form
+            } else {
+            try catch wrong username/password
+            }
+             
+             */
+            this.Hide();
+            MainForm mainForm = new MainForm();
+            mainForm.ShowDialog();
+        }
+    }
+}
